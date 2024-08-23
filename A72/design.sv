8e7e12e80843be4eff6d48ce7553bf4bf72ae2ac
Sim Jia Jun <118655979+JJ07-ops@users.noreply.github.com>
2024-08-23 11:59:27 +0800

    Create design.sv

---
+++ A72/design.sv
@@ -0,0 +1,16 @@
+//design code given in the question
+
+module dff (
+input clk,rst,din,
+output reg dout  
+);
+  
+  always_ff@(posedge clk)
+    begin
+      if(rst)
+        dout <= 0;
+      else
+        dout <= din; 
+    end
+  
+  endmodule
