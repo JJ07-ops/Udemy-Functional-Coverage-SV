866d966a3d974916bd78a122e87e4342b1792594
Sim Jia Jun <118655979+JJ07-ops@users.noreply.github.com>
2024-08-23 10:16:21 +0800

    Create design.sv

---
+++ A51/design.sv
@@ -0,0 +1,17 @@
+//design code given in the question
+
+module mux_2 (
+  input [1:0] a,b,
+  input sel,
+  output reg [1:0] y 
+);
+  
+always_comb
+  begin
+    if(sel) 
+      y = a;
+    else
+      y = b;
+  end
+  
+  endmodule
