62515081e5b8623148304d7d40ba4d7b7671084b
Sim Jia Jun <118655979+JJ07-ops@users.noreply.github.com>
2024-08-23 13:18:35 +0800

    Create design.sv

---
+++ A82/design.sv
@@ -0,0 +1,23 @@
+//design code given in the question
+
+module top (
+  input [2:0] a, b,
+  input rst,
+  input feed,
+  output [2:0] y,
+  output done
+);
+  
+/*
+ 
+User Logic here
+Do not add anything
+..............
+............
+...........
+.........
+.......
+ 
+*/
+  
+  endmodule
